98745ad093087901b82c416682922343d8b79cb9
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-17 09:55:24 -0500

    Add nextJs config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  
-}
+  env: {
+    customKey: 'customValue',
+  },
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://grajalesu.vercel.app/',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

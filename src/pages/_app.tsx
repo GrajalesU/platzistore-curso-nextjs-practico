ddd3d77cc4b8573ed4074bae1c5d16fbc4af2bd5
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-17 23:53:10 -0500

    Fix google analytics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import Header from '@components/Header';
 import AppContext from '@context/AppContext';
 import useInitialState from '@hooks/useInitialState';
-import '../styles/global.css';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const initialState = useInitialState();
 
   return (
     <AppContext.Provider value={initialState}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BXC77X5Y4T" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-BXC77X5Y4T');`}
+      </Script>
       <Header />
       <Component {...pageProps} />
     </AppContext.Provider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,17 +3,7 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BXC77X5Y4T"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-BXC77X5Y4T');`,
-          }}
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />

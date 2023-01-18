2e019afed5f8eee1c0c8fdbc4005925c385dc2ec
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-17 23:30:30 -0500

    Add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BXC77X5Y4T"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-BXC77X5Y4T');`,
+          }}
+        />
       </Head>
       <body>
         <Main />

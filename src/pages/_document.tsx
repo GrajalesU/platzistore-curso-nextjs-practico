8fee27e2d61a6457d94a5f4e12826a77752b7977
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-01-17 23:54:34 -0500

    Add meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      </Head>
       <body>
         <Main />
         <NextScript />
